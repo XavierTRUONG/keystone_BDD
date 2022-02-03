5afaffb0961cf8647d8f3d4a00f113895d4d3a12
Xavier TRUONG <xaviertruongngochuy@gmail.com>
2022-02-03 16:40:35 +0100

    modification des procedures

--- Database/procedures.sql
+++ Database/procedures.sql
@@ -4,87 +4,150 @@ DELIMITER //
 
 -- insertNewUser -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-CREATE OR REPLACE PROCEDURE insertNewUser (IN p_prenom VARCHAR(255),IN p_nom VARCHAR(255), IN p_email VARCHAR(255), 
-IN p_password VARCHAR(255))
+CREATE OR REPLACE PROCEDURE insertNewUser 
+(
+    IN p_prenom VARCHAR(255),
+    IN p_nom VARCHAR(255), 
+    IN p_email VARCHAR(255), 
+    IN p_password VARCHAR(255)
+)
 BEGIN
-    INSERT INTO `UTILISATEUR` (
-    `PRENOM_UTILISATEUR`,
-    `NOM_UTILISATEUR`,
-    `MAIL_UTILISATEUR`,
-    `PASSWORD_UTILISATEUR`)
-     VALUES (p_prenom, p_nom, p_email, p_password);
+    INSERT INTO UTILISATEUR 
+    (
+        PRENOM_UTILISATEUR,
+        NOM_UTILISATEUR,
+        MAIL_UTILISATEUR,
+        PASSWORD_UTILISATEUR
+    )
+    VALUES 
+    (
+        p_prenom, 
+        p_nom, 
+        p_email, 
+        p_password
+    );
 END //
 
 -- insertNewImage -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-CREATE OR REPLACE PROCEDURE insertNewImage (IN p_description VARCHAR(255),IN p_ville VARCHAR(50), IN p_pays VARCHAR(50), 
-IN p_code_postal INT, IN p_image LONGBLOB)
+CREATE OR REPLACE PROCEDURE insertNewImage 
+(
+    IN p_description VARCHAR(255),
+    IN p_ville VARCHAR(50), 
+    IN p_pays VARCHAR(50), 
+    IN p_code_postal INT, 
+    IN p_nom_image VARCHAR(255)
+)
 BEGIN
-    INSERT INTO `IMAGE` (
-    `DESCRIPTION_IMAGE`,
-    `VILLE_IMAGE`,
-    `PAYS_IMAGE`,
-    `CODE_POSTAL_IMAGE`,
-    `IMAGE_TOURNAGE`)
-     VALUES (p_prenom, p_nom, p_email, p_password,LOAD_FILE(p_image));
+    INSERT INTO 
+        `IMAGE` 
+    (
+        DESCRIPTION_IMAGE,
+        VILLE_IMAGE,
+        PAYS_IMAGE,
+        CODE_POSTAL_IMAGE,
+        NOM_IMAGE
+    )
+    VALUES 
+    (
+        p_description,
+        p_ville, 
+        p_pays, 
+        p_code_postal, 
+        p_nom_image
+    );
 END //
 
--- bookReservation -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- updateImageById -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
-CREATE OR REPLACE PROCEDURE  (IN p_date DATE, IN p_organisateur BIGINT, IN p_placeparking BIGINT, IN p_repas BIGINT, IN p_salle BIGINT, IN p_utilisateur BIGINT)
+CREATE OR REPLACE PROCEDURE updateImageById 
+(
+    IN p_id_image BIGINT, 
+    IN p_description VARCHAR(255),
+    IN p_ville VARCHAR(50), 
+    IN p_pays VARCHAR(50), 
+    IN p_code_postal INT, 
+    IN p_nom_image VARCHAR(255)
+)
 BEGIN
-    INSERT INTO `CHERCHER`(
-    `DATE_REUNION` ,
-    `ID_ORGANISATEUR` ,
-    `ID_PLACE_PARKING` ,
-    `ID_REPAS` ,
-    `ID_SALLE` ,
-    `ID_UTILISATEUR`) VALUES (p_date, p_organisateur, p_placeparking, p_repas, p_salle, p_utilisateur);
+    UPDATE 
+        `IMAGE`
+    SET 
+        DESCRIPTION_IMAGE = p_description,
+        VILLE_IMAGE = p_ville,
+        PAYS_IMAGE = p_pays,
+        CODE_POSTAL_IMAGE = p_code_postal,
+        NOM_IMAGE = p_nom_image
+    WHERE 
+        ID_IMAGE = p_id_image;
 END //
 
--- updateReservation -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- updateUserById -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-CREATE OR REPLACE PROCEDURE updateReservation (IN p_organisateur BIGINT, IN p_date DATE, IN p_placeparking BIGINT, IN p_repas BIGINT,
-IN p_salle BIGINT)
+CREATE OR REPLACE PROCEDURE updateUserById 
+(
+    IN p_id_utilisateur BIGINT,
+    IN p_prenom VARCHAR(255),
+    IN p_nom VARCHAR(255), 
+    IN p_email VARCHAR(255), 
+    IN p_password VARCHAR(255)
+)
 BEGIN
-    UPDATE RESERVER
-    SET DATE_REUNION = p_date,
-    ID_PLACE_PARKING = p_placeparking,
-    ID_REPAS = p_repas,
-    ID_SALLE = p_salle
-    WHERE ID_ORGANISATEUR = p_organisateur;
+    UPDATE 
+        UTILISATEUR
+    SET 
+        PRENOM_UTILISATEUR = p_prenom,
+        NOM_UTILISATEUR = p_nom,
+        MAIL_UTILISATEUR = p_email,
+        PASSWORD_UTILISATEUR = p_password,
+    WHERE 
+        ID_UTILISATEUR = p_id_utilisateur;
 END //
 
--- deleteBookReservation -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- deleteImage -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-CREATE OR REPLACE PROCEDURE deleteImage (IN p_organisateur BIGINT)
+CREATE OR REPLACE PROCEDURE deleteImage 
+(
+    IN p_id_image BIGINT
+)
 BEGIN
     DELETE 
-    FROM RESERVER
-    WHERE DATE_REUNION = p_date 
-    AND ID_ORGANISATEUR = p_organisateur;
+    FROM 
+        `IMAGE`
+    WHERE 
+        ID_IMAGE = p_id_image;
 END//
 
--- identificationUtilisateur-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- deleteUser -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-CREATE OR REPLACE PROCEDURE identificationUtilisateur (IN p_email VARCHAR(255),IN p_password VARCHAR(255))
+CREATE OR REPLACE PROCEDURE deleteImage 
+(
+    IN p_id_utilisateur BIGINT
+)
 BEGIN
-SELECT NOM_UTILISATEUR, PRENOM_UTILISATEUR, MAIL_UTILISATEUR, TELEPHONE_UTILISATEUR, PASSWORD_UTILISATEUR, NOM_LIGUE
-FROM UTILISATEUR
-INNER JOIN TYPE_LIGUE ON UTILISATEUR.ID_TYPE_LIGUE = TYPE_LIGUE.ID_TYPE_LIGUE
-WHERE MAIL_UTILISATEUR = p_email
-AND PASSWORD_UTILISATEUR = p_password;
-END //
+    DELETE 
+    FROM 
+        UTILISATEUR
+    WHERE 
+        ID_UTILISATEUR = p_id_utilisateur;
+END//
 
--- identificationAdmin -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- getAllUser -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-CREATE OR REPLACE PROCEDURE identificationAdmin (IN p_email VARCHAR(255),IN p_password VARCHAR(255))
+CREATE OR REPLACE PROCEDURE getAllUser 
+(
+    IN p_email VARCHAR(255),
+    IN p_password VARCHAR(255)
+)
 BEGIN
 SELECT *
-FROM ADMINISTRATEUR
-WHERE MP_ADMIN = Sha1(p_password);
+FROM 
+    UTILISATEUR
 END //
+
+-- getImageByName -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 
 
 
