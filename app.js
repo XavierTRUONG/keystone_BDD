d5ce1552fce9abcc303e2b212f3f94c81a05ad67
Xavier TRUONG <xaviertruongngochuy@gmail.com>
2022-02-04 12:03:26 +0100

    toutes les nouvelles modifs

---
+++ app.js
@@ -0,0 +1,23 @@
+require("dotenv").config({ path: `./config/${process.env.NODE_ENV}.env` })
+
+
+const express = require("express") // importe le paquet express
+const cors = require("cors"); // importe le paquet cors
+
+
+const app = express();
+
+
+app.use(cors()) // permet de parcer(analyser) toutes les requêtes en cors
+app.use(express.json()); // permet de parcer(analyser) toutes les requêtes en json
+
+
+const todosRoute = require("./routes/index"); // import ce qui est exporter dans l'index
+
+
+app.use("/api", todosRoute); // rajoute sur le chemin un router supplémentaire et un préfixe /api avant /testRoute
+app.get("/api", (_,res) => res.send("Hello from keystone BDD v1")); // Ecoute la methode GET et la route "/"
+app.listen(process.env.PORT, () => console.log(`Backend is running on PORT ${process.env.PORT}`)); // Démarrer le serveur et écouter un port précis
+
+
+module.exports = app;
