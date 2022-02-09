66e655429ff35f157e658447102820e54827f397
Xavier TRUONG <xaviertruongngochuy@gmail.com>
2022-02-09 15:55:52 +0100

    Ajout du dossier frontend

--- Backend/index.js
+++ Backend/index.js
@@ -7,9 +7,9 @@ const cors = require('cors'); // importe le paquet cors
 
 
 const app = express();
-const upload = multer({dest: 'public'});
+const upload = multer({dest: './public/images'});
 
-app.use('/static',express.static('./public/images/'));
+app.use('/static',express.static('./public/images'));
 app.use(cors()); // permet de parcer(analyser) toutes les requêtes en cors
 app.use(express.json()); // permet de parcer(analyser) toutes les requêtes en json
 
@@ -20,7 +20,7 @@ const todosRoute = require('./routes/index'); // import ce qui est exporter dans
 app.use('/api', todosRoute); // rajoute sur le chemin un router supplémentaire et un préfixe /api avant /todosRoute
 app.get('/api', (_,res) => res.send("Hello from keystone BDD v1")); // Ecoute la methode GET et la route "/"
 app.post('/uploadFile', upload.single('avatar'), ( _, res) => {
-    res.send("208")
+    res.send("success")
 });
 app.listen(process.env.PORT, () => console.log(`Backend is running on PORT ${process.env.PORT}`)); // Démarrer le serveur et écouter un port précis
 
