ea87fa5f99076bf163caf019609c2429f7698971
Xavier TRUONG <xaviertruongngochuy@gmail.com>
2022-02-15 16:37:01 +0100

    nouvelle version 1.2

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,12 +1,15 @@
 import Search from '../../components/Search';
+import Burger from '../../components/BurgerIcon';
 import './index.css';
 
 
 const Home = () => {
     return (
         <div className="home">
-            <h2 className='titre'></h2>
+            <h2 className='titre'>KEYSTONE FILMS</h2>
             <Search/>
+            <Burger/>
+            <div className="ligne-home"></div>
         </div>
     )
 }
