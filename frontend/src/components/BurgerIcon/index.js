7d8ff2db8dcedb9d5969ebfd77e30b792370e195
Xavier TRUONG <xaviertruongngochuy@gmail.com>
2022-04-22 23:30:39 +0200

    ajout de l'animation bouton

--- frontend/src/components/BurgerIcon/index.js
+++ frontend/src/components/BurgerIcon/index.js
@@ -2,7 +2,9 @@ import './index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
+
 const Burger = () => { 
+
     return ( 
         <div className='burger-icon'>
             <FontAwesomeIcon icon={faBars} class='logo-burger btn1'></FontAwesomeIcon>
--- frontend/src/pages/Home/home.js
+++ frontend/src/pages/Home/home.js
@@ -1,15 +1,23 @@
 import Search from '../../components/Search';
-import Burger from '../../components/BurgerIcon';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { useState } from "react";
 import './home.css';
 
 
 const Home = () => {
+    const [toggle, setToggle] = useState(false);
+
+    const changeState = () => {
+        setToggle(!toggle);
+    }
     return (
         <div className="home">
             <h2 className='titre'>KEYSTONE FILMS</h2>
             <>
                 <Search/>
-                <Burger/>
+                <div className={toggle ? "box animated" : "box"}></div>
+                <button onClick={changeState}><FontAwesomeIcon icon={faBars} class='logo-burger btn1'></FontAwesomeIcon></button>
             </>
             <div className="ligne-home"></div>
         </div>
